fa327eeec4eb8332dec141b72703dcc498fe7257
Pablo Riedemann <zoserx@gmail.com>
2015-11-11 22:59:19 -0300

    Initial Commit

---
+++ AppLauncher/Configuration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLauncher
+{
+    public class Configuration
+    {
+        private static Configuration _instance;
+
+        public static Configuration Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new Configuration();
+                }
+                return _instance;
+            }
+        }
+
+        private Configuration()
+        {
+
+        }
+
+ 
+    }
+}
---
+++ AppLauncher/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Configuration;
+
+namespace AppLauncher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer _mouseMoveTimer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Mouse.OverrideCursor = Cursors.None;
+            _mouseMoveTimer = new DispatcherTimer();
+            _mouseMoveTimer.Tick += new EventHandler(MouseMoveTimerTick);
+            RestartMouseTimer();
+
+            var a = ConfigurationManager.AppSettings["Application Name"];
+            Console.WriteLine("sda");
+        }
+        private void ButtonExitOnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void WindowOnMouseMove(object sender, MouseEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Arrow;
+            RestartMouseTimer();
+        }
+
+        private void RestartMouseTimer()
+        {
+            _mouseMoveTimer.Interval = new TimeSpan(0, 0, 2);
+            if (_mouseMoveTimer.IsEnabled)
+            {
+                _mouseMoveTimer.Stop();
+            }
+            else
+            {
+                _mouseMoveTimer.Start();
+            }
+ 
+        }
+
+        private void MouseMoveTimerTick(object sender, EventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.None;
+        }
+    }
+}
